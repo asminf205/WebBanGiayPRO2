a52b3acfe67a8ee243fb822026b6cb8f9717f5cc
khonvt <khonvtps04823@fpt.edu.vn>
2017-10-21 11:31:27 +0700

    update code

--- src/poly/bean/ChiTietHoaDon.java
+++ src/poly/bean/ChiTietHoaDon.java
@@ -1,5 +1,50 @@
 package poly.bean;
 
+import javax.persistence.AssociationOverride;
+import javax.persistence.AssociationOverrides;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@AssociationOverrides({
+	@AssociationOverride(joinColumns = @JoinColumn(name="mahoadon"),name="identity.hoadon"),
+	@AssociationOverride(joinColumns = @JoinColumn(name="ma"),name="identity.sp"),
+})
+@Entity
+@Table(name="chitiethoadon")
 public class ChiTietHoaDon {
 
+	@EmbeddedId
+	ChiTietHoaDon_Identity identity = new ChiTietHoaDon_Identity();
+	
+	@Column(name="soluong")
+	int soluong;
+	
+	public ChiTietHoaDon(){}
+
+	public int getSoluong() {
+		return soluong;
+	}
+
+	public void setSoluong(int soluong) {
+		this.soluong = soluong;
+	}
+	
+	public HoaDon getHoadon() {
+		return identity.getHoadon();
+	}
+
+	public void setHoadon(HoaDon hoadon) {
+		this.identity.hoadon = hoadon;
+	}
+
+	public SanPham getSp() {
+		return identity.getSp();
+	}
+
+	public void setSp(SanPham sp) {
+		this.identity.sp = sp;
+	}
 }
--- src/poly/bean/ChiTietHoaDon_Identity.java
+++ src/poly/bean/ChiTietHoaDon_Identity.java
@@ -1,5 +1,32 @@
 package poly.bean;
 
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
 public class ChiTietHoaDon_Identity {
 
+	@ManyToOne
+	public HoaDon hoadon;
+	
+	@ManyToOne
+	public SanPham sp;
+
+	public HoaDon getHoadon() {
+		return hoadon;
+	}
+
+	public void setHoadon(HoaDon hoadon) {
+		this.hoadon = hoadon;
+	}
+
+	public SanPham getSp() {
+		return sp;
+	}
+
+	public void setSp(SanPham sp) {
+		this.sp = sp;
+	}
+	
+	
 }
