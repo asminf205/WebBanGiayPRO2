3a78feb070bae6f3984b721b99b9893bf8344150
tkvfpt <tienkhonvuong@gmail.com>
2017-10-26 13:20:13 +0700

    Add files via upload

---
+++ src/poly/dao/KhachHangDAO.java
@@ -0,0 +1,21 @@
+
+package poly.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import poly.bean.KhachHang;
+import poly.dao.generic.GenericDAO;
+
+@Component("khachhangDAO")
+@Transactional
+public class KhachHangDAO extends GenericDAO<KhachHang>{
+
+	@Override
+	public List<KhachHang> getAll() {
+		setClassType(KhachHang.class);
+		return super.getAll();
+	}	
+}
