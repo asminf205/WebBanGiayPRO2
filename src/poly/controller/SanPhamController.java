4f0f9ee3c85ccd9f318a2277d7d6e6e60e29a741
khonvt <khonvtps04823@fpt.edu.vn>
2017-10-27 21:41:42 +0700

    update

--- src/poly/controller/SanPhamController.java
+++ src/poly/controller/SanPhamController.java
@@ -1,5 +1,7 @@
 package poly.controller;
 
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,74 +12,75 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import poly.bean.SanPham;
 import poly.dao.generic.IGenericDAO;
+import poly.utils.CommonUtils;
 
 @Controller
 public class SanPhamController {
-	
-	@Autowired	
+
+	@Autowired
 	IGenericDAO<SanPham> sanphamDAO;
 
 	/**
 	 * Show All Product
-	 * 	
+	 * 
 	 * @param model
 	 * @param request
 	 * @return
 	 */
-	@RequestMapping(value="/",method=RequestMethod.GET)
-	public String laysanpham(Model model,HttpServletRequest request){
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String laysanpham(Model model, HttpServletRequest request) {
 		model.addAttribute("list", sanphamDAO.getAll());
 		return "quanlysanpham";
 	}
-	
+
 	@RequestMapping(value = "/edit")
-	public String editSP(HttpServletRequest request, Model model){
-		int ma= Integer.parseInt(request.getParameter("ma"));
+	public String editSP(HttpServletRequest request, Model model) {
+		int ma = Integer.parseInt(request.getParameter("ma"));
 		SanPham sp = sanphamDAO.getObj(ma);
-        request.setAttribute("SP", sp);
-        return "editproduct";
+		request.setAttribute("SP", sp);
+		return "editproduct";
 	}
-	
-	@RequestMapping(value="/themsanpham")
-		public String themsanpham(Model model,HttpServletRequest request){
-		try {
-			
-			if (request.getParameter("action").equalsIgnoreCase("Insert")) {						
-				String ten = request.getParameter("txtTen");
-				int gia = Integer.parseInt(request.getParameter("txtGia"));
-				String hinh = request.getParameter("txtHinh");
-				String soluonga = request.getParameter("txtSoluong");
-				int soluong = Integer.parseInt(soluonga);
-				String mau = request.getParameter("txtMau");
-				String cogiay = request.getParameter("txtSize");
-				int size = Integer.parseInt(cogiay);
-				String hangsx = request.getParameter("txtHang");
-				sanphamDAO.saveObject(new SanPham( ten, gia, hinh, soluong,mau,size, hangsx));						
-				return "redirect:/";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
+
+	@RequestMapping(value = "/themsanpham")
+	public String themsanpham(Model model, HttpServletRequest request) {
+		if (request.getParameter("action").equalsIgnoreCase("Insert")) {
+			String ten = request.getParameter("txtTen");
+			int gia = Integer.parseInt(request.getParameter("txtGia"));
+			String hinh = request.getParameter("txtHinh");
+			String soluonga = request.getParameter("txtSoluong");
+			int soluong = Integer.parseInt(soluonga);
+			String mau = request.getParameter("txtMau");
+			String cogiay = request.getParameter("txtSize");
+			int size = Integer.parseInt(cogiay);
+			String hangsx = request.getParameter("txtHang");
+			sanphamDAO.saveObject(new SanPham(ten, gia, hinh, soluong, mau, size, hangsx));
+			return "redirect:/";
+		}
 		return "themsanpham";
 	}
-	@RequestMapping(value="/suasanpham", method=RequestMethod.GET)
-	public String suasanpham(Model model,HttpServletRequest request){
-			int ma= Integer.parseInt(request.getParameter("txtMa"));
-			SanPham sp = sanphamDAO.getObj(ma);
-			sp.setTen(request.getParameter("txtTen"));
-			sp.setGia(Integer.parseInt(request.getParameter("txtGia")));
-			sp.setHinh("abc");
-			String soluonga = request.getParameter("txtSoluong");
-			sp.setSoluong(Integer.parseInt(soluonga));
-			sp.setMau(request.getParameter("txtMau"));
-			String cogiay = request.getParameter("txtSize");
-			sp.setSize(Integer.parseInt(cogiay));
-			sp.setHangsx(request.getParameter("txtHang"));			
-			sanphamDAO.updateObject(sp);						
-			return "redirect:/";
-}
+
+	@RequestMapping(value = "/suasanpham", method = RequestMethod.GET)
+	public String suasanpham(Model model, HttpServletRequest request) {		
+		request.getParameterMap().values().iterator().next();
+		
+		int ma = Integer.parseInt(request.getParameter("txtMa"));
+		SanPham sp = sanphamDAO.getObj(ma);
+		CommonUtils.settingAttributeForObject(sp, request);
+		sp.setTen(request.getParameter("txtTen"));
+		sp.setGia(Integer.parseInt(request.getParameter("txtGia")));
+		sp.setHinh("abc");
+		String soluonga = request.getParameter("txtSoluong");
+		sp.setSoluong(Integer.parseInt(soluonga));
+		sp.setMau(request.getParameter("txtMau"));
+		String cogiay = request.getParameter("txtSize");
+		sp.setSize(Integer.parseInt(cogiay));
+		sp.setHangsx(request.getParameter("txtHang"));
+		sanphamDAO.updateObject(sp);
+		return "redirect:/";
+	}
+
 	@RequestMapping("/search")
-	public String search(){
+	public String search() {
 		return "quanlysanpham";
 	}
 }
