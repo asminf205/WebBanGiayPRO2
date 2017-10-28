0ad508816ce138988b420d03df3f41845305aecd
khonvt <khonvtps04823@fpt.edu.vn>
2017-10-29 01:06:19 +0700

    update constant

--- src/poly/constants/SneakerGlobalConstant.java
+++ src/poly/constants/SneakerGlobalConstant.java
@@ -5,7 +5,7 @@ public class SneakerGlobalConstant {
 	private SneakerGlobalConstant(){
 		throw new IllegalStateException("Constant Class");
 	}
-
+	/* Common Constants */
 	public static final String OBJECT_ID = "ma";
 	public static final String OBJECT_NAME = "ten";
 }
